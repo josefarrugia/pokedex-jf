d9ccf414a924d4385e371c85f6d53126452e2f2f
Jose Farrugia <jose@josefarrugia.com>
2024-06-07 07:37:38 +0100

    feat: add initial setup for apollo client

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,37 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://beta.pokeapi.co/graphql/v1beta',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
 
 // Components
 import App from './App.vue'
 
 // Composables
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-const app = createApp(App)
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 registerPlugins(app)
 
