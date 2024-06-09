eb6b8b3cb63d45939542fb81926415f98ab4b0eb
Jose Farrugia <jose@josefarrugia.com>
2024-06-09 15:20:36 +0100

    feat: add components

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,11 +5,12 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module 'vue' {
+declare module "vue" {
   export interface GlobalComponents {
-    AppFooter: typeof import('./components/AppFooter.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
+    AppDialog: (typeof import("./components/AppDialog.vue"))["default"];
+    AppFooter: (typeof import("./components/AppFooter.vue"))["default"];
+    AppHeader: (typeof import("./components/AppHeader.vue"))["default"];
+    RouterLink: (typeof import("vue-router"))["RouterLink"];
+    RouterView: (typeof import("vue-router"))["RouterView"];
   }
 }
