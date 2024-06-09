475304b5fc0938ae275825b8039a497faf2dbc40
Jose Farrugia <jose@josefarrugia.com>
2024-06-09 15:10:12 +0100

    style: update spacing

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import {useRouter} from "vue-router";
+import { useRouter } from "vue-router";
 
 const router = useRouter();
 
@@ -7,27 +7,41 @@ const drawer = ref(false);
 </script>
 
 <template>
-  <v-app-bar
-    color="red"
-  >
-    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-app-bar color="red">
+    <v-app-bar-nav-icon
+      variant="text"
+      @click.stop="drawer = !drawer"
+    ></v-app-bar-nav-icon>
 
     <v-toolbar-title>Pokedex–<small>JF</small></v-toolbar-title>
     <v-spacer></v-spacer>
 
-    <v-btn icon="mdi-home-variant-outline" variant="text" @click="router.push({ name: '/'})"></v-btn>
-    <v-btn icon="mdi-tag-heart-outline" variant="text" @click="router.push({ name: '/pokemon/favourites'})"></v-btn>
+    <v-btn
+      icon="mdi-home-variant-outline"
+      variant="text"
+      @click="router.push({ name: '/' })"
+    ></v-btn>
+    <v-btn
+      icon="mdi-tag-heart-outline"
+      variant="text"
+      @click="router.push({ name: '/pokemon/favourites' })"
+    ></v-btn>
   </v-app-bar>
 
-  <v-navigation-drawer
-    v-model="drawer"
-    location="left"
-    temporary
-  >
+  <v-navigation-drawer v-model="drawer" location="left" temporary>
     <v-list>
-      <v-list-item prepend-icon="mdi-home-variant-outline" title="Home" value="home" :to="{ name: '/'}"></v-list-item>
-      <v-list-item prepend-icon="mdi-tag-heart-outline" title="Favourites" value="favourites"
-                   :to="{ name: '/pokemon/favourites'}"></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-home-variant-outline"
+        title="Home"
+        value="home"
+        :to="{ name: '/' }"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-tag-heart-outline"
+        title="Favourites"
+        value="favourites"
+        :to="{ name: '/pokemon/favourites' }"
+      ></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
