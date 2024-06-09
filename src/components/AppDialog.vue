8543152a0d62f277155dcca50fba2b996ddcd69b
Jose Farrugia <jose@josefarrugia.com>
2024-06-09 14:51:09 +0100

    feat: add AppDialog

---
+++ src/components/AppDialog.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import {useAppStore} from "@/stores/app";
+
+const store = useAppStore();
+</script>
+
+<template>
+  <v-dialog
+    v-model="store.getShowDialog"
+    max-width="320"
+    persistent
+  >
+    <v-list
+      class="py-2"
+      color="primary"
+      elevation="12"
+      rounded="lg"
+    >
+      <v-list-item
+        :prepend-icon="store.getDialogInfo.icon"
+        :title="store.getDialogInfo.title"
+      >
+        <template v-slot:prepend>
+          <div class="pe-4">
+            <v-icon color="success" size="x-large"></v-icon>
+          </div>
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-dialog>
+</template>
